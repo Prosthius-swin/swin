509a577ce84aa6532b08db854abe5b921950a2ae
Prosthius-swin <103919468@student.swin.edu.au>
2022-03-04 15:15:56 +1100

    Completed

---
+++ 530/week-02/4.cs
@@ -0,0 +1,30 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+        //A program that asks the user to enter a number. The program then counts from 0 to that number.
+        string number = "0";
+        int counter = 0;
+		int numberInt;
+
+        Console.WriteLine("Enter a number");
+        number = Console.ReadLine();
+		numberInt = int.Parse(number);
+		
+        if (numberInt > 0) {
+            while(counter <= numberInt) {
+                Console.WriteLine(counter);
+                counter = counter + 1;
+                }
+        } else if (numberInt < 0) {
+            while(counter >= numberInt) {
+                Console.WriteLine(counter);
+                counter = counter - 1;
+                }
+            } else {
+                Console.WriteLine(numberInt);
+            }
+	}
+}
