21659b420573599bd1606180825d69e1f8a7d6d2
Prosthius-swin <103919468@student.swin.edu.au>
2022-03-04 13:13:31 +1100

    Completed

---
+++ 530/week-02/count-0-to-20-by-2s.cs
@@ -0,0 +1,14 @@
+using System;
+					
+public class Program
+{
+	public static void Main()
+	{
+        int counter = 0;
+
+        while (counter <=20) {
+            Console.WriteLine(counter);
+            counter = counter + 2;
+        }
+	}
+}
