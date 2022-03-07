eafa1eb3594be3aa11539e6b516d220d96454884
Prosthius-swin <103919468@student.swin.edu.au>
2022-03-08 10:58:47 +1100

    Fixed formatting Added else statement

--- intro-to-programming/430-apply-introductory-object-oriented-language-skills/week-02/3.cs
+++ intro-to-programming/430-apply-introductory-object-oriented-language-skills/week-02/3.cs
@@ -4,15 +4,18 @@ public class Program
 {
 	public static void Main()
 	{
-        //A program that asks the user to guess a number. The magic number is 5. 
-        //If they get it wrong, the program asks for the number again.
-        string guess = "0";
+            //A program that asks the user to guess a number. The magic number is 5. 
+            //If they get it wrong, the program asks for the number again.
+            string guess = "0";
 		
-        while(guess != "5") {
-        Console.WriteLine("Guess the number");
-		guess = Console.ReadLine();
-            } if (guess == "5") {
-		Console.WriteLine("Correct!");
-		} 
+            while(guess != "5") {
+                  Console.WriteLine("Guess the number");
+		      guess = Console.ReadLine();
+                  if (guess == "5") {
+		            Console.WriteLine("Correct!");
+		      } else {
+                        Console.WriteLine("Wrong, try again");
+                  }
+            }               
 	}
 }
