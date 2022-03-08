416e13e4d8fb071685383ab3936a23681eb16231
Prosthius-swin <103919468@student.swin.edu.au>
2022-03-08 15:33:01 +1100

    Completed with FOR and WHILE loops

---
+++ intro-to-programming/430-apply-introductory-object-oriented-language-skills/week-02/nested-repetition/2.cs
@@ -0,0 +1,49 @@
+//Create a C# program that creates the below output and copy and paste your code as the answer.
+/*
+xxxx
+xxx
+xx
+x
+*/
+
+//Completed with for loops
+char x = 'x';
+
+for (int i = 0; i < 4; i++) {
+    Console.Write(x);
+} Console.WriteLine();
+for (int i = 0; i < 3; i++) {
+    Console.Write(x);
+} Console.WriteLine();
+for (int i = 0; i < 2; i++) {
+    Console.Write(x);
+} Console.WriteLine();
+for (int i = 0; i < 1; i++) {
+    Console.Write(x);
+}
+
+
+//Completed with while loops
+int counter = 0;
+char x = 'x';
+
+while(counter < 4) {
+    counter++;
+    Console.Write(x);    
+} Console.WriteLine();
+counter = 0;
+while(counter < 3) {
+    counter++;
+    Console.Write(x);    
+} Console.WriteLine();
+counter = 0;
+while(counter < 2) {
+    counter++;
+    Console.Write(x);    
+} Console.WriteLine();
+counter = 0;
+while(counter < 1) {
+    counter++;
+    Console.Write(x);    
+} Console.WriteLine();
+counter = 0;
