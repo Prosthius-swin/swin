1aa7d4a7766a2f468da3cb6f8b20f121f8862101
Prosthius-swin <103919468@student.swin.edu.au>
2022-04-02 16:10:04 +1100

    Runs and re-runs except for logins

--- database-analysis-and-design/506-design-databases/week-5/5.sql
+++ database-analysis-and-design/506-design-databases/week-5/5.sql
@@ -1,7 +1,19 @@
+USE master;
+GO
+
+DROP DATABASE IF EXISTS week_5_9468;
+GO
+
 CREATE DATABASE week_5_9468;
 GO
+
 USE week_5_9468
 GO
+
+DROP TABLE IF EXISTS employee; 
+DROP TABLE IF EXISTS office;
+DROP TABLE IF EXISTS department
+DROP TABLE IF EXISTS building;
 
 CREATE TABLE Building (
     BuildingCode NVARCHAR(4),
@@ -50,6 +62,38 @@ CREATE TABLE Employee (
 );
 GO
 
+INSERT INTO Building (BuildingCode, [Description]) VALUES
+('DGGP', 'Dod & Gy Google Plex'),
+('DGL', 'Dod & Gy Louvre'),
+('DGW', 'Dod & Gy Whitehouse');
+
+INSERT INTO Department (DeptCode, DeptName) VALUES
+('SNM', 'Sales and Marketing'),
+('FULL', 'Fulfillment'),
+('TAD', 'Tax Avoidance Department'),
+('PROC', 'Procurement'),
+('HR', 'Humane Redundancies');
+
+INSERT INTO Office (BuildingCode, OfficeNo, NumDesks, Phone) VALUES
+('DGGP', 101, 3, '(03)9555-1234'),
+('DGGP', 102, 2, '(03)9555-1235'),
+('DGL', 512, 4, '(03)9555-1236'),
+('DGL', 514, 2, '(03)9555-1237'),
+('DGW', 53, 1, '(03)9555-1238');
+
+INSERT INTO Employee (EmpID, FirstName, Surname, Email, DeptCode, BuildingCode, OfficeNo) VALUES
+(10001, 'Pat', 'Cummins', 'patty@acb.com.au', 'SNM', 'DGGP', 101),
+(10002, 'Tim', 'Paine', 'tim@acb.com.au', 'SNM', 'DGL', 512),
+(10003, 'Steve', 'Smith', 'steve@acb.com.au', 'FULL', 'DGW', 53),
+(10004, 'Shane', 'Watson', 'shape@acb.com.au', 'PROC', 'DGL', 514),
+(10005, 'Meg', 'Lanning', 'meg@acb.com.au', 'HR', 'DGL', 512);
+GO
+
+
+DROP VIEW IF EXISTS EmployeesByDept;
+DROP VIEW IF EXISTS BuildingDeskNumbers;
+GO
+
 CREATE VIEW EmployeesByDept 
 AS
 SELECT E.Surname, E.FirstName, D.DeptName
@@ -65,6 +109,12 @@ FROM Office O
 GROUP BY O.BuildingCode
 GO
 
+DROP LOGIN admin9468;
+DROP USER IF EXISTS admin9468;
+DROP LOGIN api9468;
+DROP USER IF EXISTS api9468;
+GO
+
 CREATE LOGIN admin9468 WITH PASSWORD = 'Password1!';
 CREATE USER admin9468;
 CREATE LOGIN api9468 WITH PASSWORD = 'Password1!';
@@ -75,3 +125,5 @@ EXEC sp_addrolemember 'db_owner', 'admin9468';
 EXEC sp_addrolemember 'db_datareader', 'api9468';
 EXEC sp_addrolemember 'db_datareader', 'api9468';
 GO
+
+SELECT * FROM Employee;
