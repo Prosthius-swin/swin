dce3d0a039ed3f77672fd6b8e905a2f3df0613fe
Prosthius-swin <103919468@student.swin.edu.au>
2022-04-02 16:22:36 +1100

    Completed

---
+++ intro-to-programming/430-apply-introductory-object-oriented-language-skills/arrays-and-lists-task/1/Program.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+
+namespace _1
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Using an Array Create a program that reads in 5 names from the console.  It then prints out "Hello <name>" for all the names entered
+            string[] names = new string[5];
+            Console.WriteLine("Enter 5 names.");
+            Console.WriteLine();
+
+            for(int i = 0; i < 5; i++) {
+                Console.Write("Name - {0} : ",i);
+                names[i] = (Console.ReadLine());
+            } 
+            Console.WriteLine("-----------------------");
+
+            foreach (string i in names) {
+                Console.WriteLine("Hello " + i);
+            }
+        }
+    }
+}
