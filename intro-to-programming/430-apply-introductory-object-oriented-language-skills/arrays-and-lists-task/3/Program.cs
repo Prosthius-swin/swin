235cd9884dd17233f62188e34da448823c972c9c
Prosthius-swin <103919468@student.swin.edu.au>
2022-04-02 17:02:45 +1100

    Started

---
+++ intro-to-programming/430-apply-introductory-object-oriented-language-skills/arrays-and-lists-task/3/Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _3
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Using a List, write a program that reads in 5 numbers. 
+            // It then asks the user for another number and outputs whether the number has already been entered.
+
+            List<int> nums = new List<num>();
+            Console.WriteLine("Enter 5 numbers:");
+            Console.WriteLine();
+
+            for(int i = 1; i < 6; i++) {
+                input = Console.ReadLine();
+                nums.Add(input);
+            } 
+            Console.WriteLine("-----------------------");
+
+            /*foreach (string i in nums) {
+                Console.WriteLine("Hello " + i);
+            }*/
+        }
+    }
+}
