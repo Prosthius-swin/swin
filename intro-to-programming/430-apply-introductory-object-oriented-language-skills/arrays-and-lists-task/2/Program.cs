efcbfc6049af78c0d16f58185191b25ae7485a26
Prosthius-swin <103919468@student.swin.edu.au>
2022-04-02 16:26:25 +1100

    Completed

---
+++ intro-to-programming/430-apply-introductory-object-oriented-language-skills/arrays-and-lists-task/2/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Linq;
+
+namespace _2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            // Using an array write a program that starts with the values 34, 5, 67, 1, 99, 34, 44, 78, 34, 0, 
+            // then adds all numbers together and outputs the total.
+
+            int[] numbers = {4, 5, 67, 1, 99, 34, 44, 78, 34, 0};
+            int sum = numbers.Sum();
+            Console.WriteLine(sum);
+
+        }
+    }
+}
