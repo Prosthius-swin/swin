c169a33d780b6bd6a7567421d46127eb8b9a8612
Prosthius-swin <103919468@student.swin.edu.au>
2022-04-03 17:19:32 +1000

    Completed Task 3

--- intro-to-programming/430-apply-introductory-object-oriented-language-skills/arrays-and-lists-task/3/Program.cs
+++ intro-to-programming/430-apply-introductory-object-oriented-language-skills/arrays-and-lists-task/3/Program.cs
@@ -10,19 +10,27 @@ namespace _3
             // Using a List, write a program that reads in 5 numbers. 
             // It then asks the user for another number and outputs whether the number has already been entered.
 
-            List<int> nums = new List<num>();
+            List<int> nums = new List<int>();
+
             Console.WriteLine("Enter 5 numbers:");
             Console.WriteLine();
 
             for(int i = 1; i < 6; i++) {
-                input = Console.ReadLine();
-                nums.Add(input);
+                Console.Write("Enter number {0}: ",i);
+                nums.Add(int.Parse(Console.ReadLine()));
             } 
+            
             Console.WriteLine("-----------------------");
 
-            /*foreach (string i in nums) {
-                Console.WriteLine("Hello " + i);
-            }*/
+            Console.Write("Enter another number: ");
+            int numSix = int.Parse(Console.ReadLine());
+
+            foreach(int check in nums) {
+                if (check == numSix) { 
+                    Console.WriteLine("The number has already been entered."); 
+                    break;
+                } Console.WriteLine("The number is not a duplicate.");
+            }
         }
     }
 }
