43f5d3942a6927d5af7d79109691b205c9ef08dc
Prosthius-swin <103919468@student.swin.edu.au>
2022-04-13 15:22:11 +1000

    Completed 5

---
+++ intro-to-programming/430-apply-introductory-object-oriented-language-skills/arrays-and-lists-task/5/Program.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+
+namespace _5
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Using a List, write a program stores words read from console, until the word "stop" is entered.  
+            //It then outputs the words entered in reverse order.
+
+            List<string> words = new List<string>();
+
+            Console.Write("Enter a word: ");
+            words.Add(Console.ReadLine());
+
+            while (!words.Contains("stop"))
+            {
+                Console.Write("Enter another word: ");
+                words.Add(Console.ReadLine());
+            }
+
+            Console.WriteLine();
+            words.RemoveAt(words.Count - 1);
+            words.Reverse();
+
+            int counter = 0;
+            foreach(string i in words)
+            {
+                counter++;
+                Console.Write($"{counter}: {i}\n");
+            }
+            Console.WriteLine();
+        }
+    }
+}
