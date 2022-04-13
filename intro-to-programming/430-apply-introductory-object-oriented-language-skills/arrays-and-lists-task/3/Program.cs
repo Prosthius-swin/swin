3cb2a1a99d573a44fcf49d805c9664b095127481
Prosthius-swin <103919468@student.swin.edu.au>
2022-04-13 14:37:14 +1000

    Completed 3

--- intro-to-programming/430-apply-introductory-object-oriented-language-skills/arrays-and-lists-task/3/Program.cs
+++ intro-to-programming/430-apply-introductory-object-oriented-language-skills/arrays-and-lists-task/3/Program.cs
@@ -9,6 +9,7 @@ namespace _3
         {
             // Using a List, write a program that reads in 5 numbers. 
             // It then asks the user for another number and outputs whether the number has already been entered.
+            int dup = 0;
 
             List<int> nums = new List<int>();
 
@@ -20,16 +21,21 @@ namespace _3
                 nums.Add(int.Parse(Console.ReadLine()));
             } 
             
-            Console.WriteLine("-----------------------");
+            Console.WriteLine("------------------------------------");
 
             Console.Write("Enter another number: ");
             int numSix = int.Parse(Console.ReadLine());
 
-            foreach(int check in nums) {
+            foreach(int check in nums) 
+            {
                 if (check == numSix) { 
                     Console.WriteLine("The number has already been entered."); 
+                    dup = 1;
                     break;
-                } Console.WriteLine("The number is not a duplicate.");
+                } 
+            } if(dup == 0)
+            {
+                Console.WriteLine("The number is not a duplicate.");
             }
         }
     }
